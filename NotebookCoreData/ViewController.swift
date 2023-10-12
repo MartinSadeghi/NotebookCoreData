637c47a8b15da1649cdb39eb6304c6a3ce1155a2
moMMo Sadeghi <37694875+moMMoSadeghi@users.noreply.github.com>
2023-10-12 15:13:57 +0200

    Initial Commit

---
+++ NotebookCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotebookCoreData
+//
+//  Created by Mommo Sadeghi on 12/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
